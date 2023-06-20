fa8423cd7ce96c1971b3635d8d0403d27b3a4cd2
FrancesGorka <francesgorka@hotmail.com>
2023-06-20 22:05:42 +0100

    finished

--- milestone_2.py
+++ milestone_2.py
@@ -1,14 +1,17 @@
 import random
 
 word_list = ['banana', 'pineapple', 'pear', 'grape', 'apple']
-print(word_list)
-
 word = random.choice(word_list)
-print(word)
 
-guess = input('Please enter a single letter: ')
-if len(guess) = 1 and guess.isalpha:
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input!")
+if __name__ == "__main__":
+    print(word_list)
+
+    
+    print(word)
+
+    guess = input('Please enter a single letter: ')
+    if len(guess) == 1 and guess.isalpha:
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input!")
     
